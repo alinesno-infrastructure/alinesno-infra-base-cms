242a1c89530a351a14aeab47ec0ac2c3e074c49e
CarrotSea233 <2733614757@qq.com>
2023-11-02 20:00:03 +0800

    feat: Add annotation

--- alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/entity/AttachmentIndexEntity.java
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/entity/AttachmentIndexEntity.java
@@ -1,10 +1,12 @@
 package com.alinesno.infra.base.cms.entity;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
-
-import cn.afterturn.easypoi.excel.annotation.Excel;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import lombok.Data;
 
 /**
  * 数据表：attachment_index 表备注：
@@ -14,6 +16,7 @@ import cn.afterturn.easypoi.excel.annotation.Excel;
  * @author luoxiaodong
  *@version 1.0.0
  */
+@Data
 @TableName("attachment_index")
 public class AttachmentIndexEntity extends InfraBaseEntity {
 
@@ -24,6 +27,8 @@ public class AttachmentIndexEntity extends InfraBaseEntity {
 	 */
 	@Excel(name = "keyid")
 	@TableField("keyid")
+	@ColumnType(length = 255)
+	@ColumnComment("keyid")
 	private String keyid;
 
 	/**
@@ -31,37 +36,7 @@ public class AttachmentIndexEntity extends InfraBaseEntity {
 	 */
 	@Excel(name = "aid")
 	@TableField("aid")
+	@ColumnType(length = 255)
+	@ColumnComment("aid")
 	private String aid;
-
-	// getter and setter methods
-
-	/**
-	 * 获取 keyid
-	 */
-	public String getKeyid() {
-		return this.keyid;
-	}
-
-	/**
-	 * 设置 keyid
-	 */
-	public AttachmentIndexEntity setKeyid(String keyid) {
-		this.keyid = keyid;
-		return this;
-	}
-
-	/**
-	 * 获取 aid
-	 */
-	public String getAid() {
-		return this.aid;
-	}
-
-	/**
-	 * 设置 aid
-	 */
-	public AttachmentIndexEntity setAid(String aid) {
-		this.aid = aid;
-		return this;
-	}
 }
--- alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/entity/BlockPrivEntity.java
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/entity/BlockPrivEntity.java
@@ -3,6 +3,9 @@ package com.alinesno.infra.base.cms.entity;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import lombok.Data;
 
 /**
  * 数据表：block_priv 表备注：
@@ -12,6 +15,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
  * @author luoxiaodong
  *@version 1.0.0
  */
+@Data
 @TableName("block_priv")
 public class BlockPrivEntity extends InfraBaseEntity {
 
@@ -21,46 +25,23 @@ public class BlockPrivEntity extends InfraBaseEntity {
 	 * 角色ID
 	 */
 	@TableField("roleid")
+	@ColumnType
+	@ColumnComment("角色ID")
 	private Integer roleid;
 
 	/**
 	 * 站点ID
 	 */
 	@TableField("siteid")
+	@ColumnType
+	@ColumnComment("站点ID")
 	private Integer siteid;
 
 	/**
 	 * 区块ID
 	 */
 	@TableField("blockid")
+	@ColumnType
+	@ColumnComment("区块ID")
 	private Integer blockid;
-
-	// getter and setter methods
-
-	public Integer getRoleid() {
-		return this.roleid;
-	}
-
-	public BlockPrivEntity setRoleid(Integer roleid) {
-		this.roleid = roleid;
-		return this;
-	}
-
-	public Integer getSiteid() {
-		return this.siteid;
-	}
-
-	public BlockPrivEntity setSiteid(Integer siteid) {
-		this.siteid = siteid;
-		return this;
-	}
-
-	public Integer getBlockid() {
-		return this.blockid;
-	}
-
-	public BlockPrivEntity setBlockid(Integer blockid) {
-		this.blockid = blockid;
-		return this;
-	}
 }
