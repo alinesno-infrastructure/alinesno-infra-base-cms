ce130818456c5a73cd03a50f6b7c75e054733b17
王明(AllenQin) <allenQin@gmail.com>
2024-10-26 00:36:53 +0800

    feat:新增 App.vue 文件，包含头部、路由视图和底部组件

---
+++ alinesno-infra-portal/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <AipHeader />
+
+  <RouterView />
+
+  <AipFooter />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+import AipHeader from './components/header.vue'
+import AipFooter from './components/footer.vue'
+
+</script>
