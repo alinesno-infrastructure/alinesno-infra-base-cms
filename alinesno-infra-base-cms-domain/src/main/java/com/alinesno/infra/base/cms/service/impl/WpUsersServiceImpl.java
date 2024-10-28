8d98fe49b7b876b9fb14857bf89ac289c509e9fc
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:实现 WpUsersServiceImpl 类，提供用户服务的基础实现。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpUsersServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpUsersEntity;
+import com.alinesno.infra.base.cms.mapper.WpUsersMapper;
+import com.alinesno.infra.base.cms.service.IWpUsersService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpUsers服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpUsersServiceImpl extends IBaseServiceImpl<WpUsersEntity, WpUsersMapper> implements IWpUsersService {
+
+}
