a303d7bbaac62f9e2d3c3cae3baf03c49c7c973e
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 添加 IWpOptionsService 接口，用于选项实体的 CRUD 操作。

---
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/service/IWpOptionsService.java
@@ -0,0 +1,13 @@
+// IWpOptionsService.java
+package com.alinesno.infra.base.cms.service;
+
+import com.alinesno.infra.base.cms.entity.WpOptionsEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * 选项服务接口
+ * 该接口继承了基础服务接口IBaseService，用于对选项实体(WpOptionsEntity)进行CRUD操作
+ */
+public interface IWpOptionsService extends IBaseService<WpOptionsEntity> {
+ 
+}
