eb6b44192d746f5da01b51fd8decdc4bb80e6970
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:新增 WpOptionsMapper 接口，用于系统选项的数据库操作。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpOptionsMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpOptionsEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 系统选项
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpOptionsMapper extends IBaseMapper<WpOptionsEntity> {
+}
