71da3276fe64d3a2c51e8a667529f05b605e82c3
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:新增用户元数据映射接口 WpUserMetaMapper

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpUserMetaMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpUserMetaEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 用户元数据
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpUserMetaMapper extends IBaseMapper<WpUserMetaEntity> {
+}
