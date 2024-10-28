e201049a900c8a4e9a448bbe9066a617cf3f5660
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:实现 WpPostMetaServiceImpl 类，提供 WpPostMeta 服务处理逻辑。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpPostMetaServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpPostMetaEntity;
+import com.alinesno.infra.base.cms.mapper.WpPostMetaMapper;
+import com.alinesno.infra.base.cms.service.IWpPostMetaService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpPostMeta服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpPostMetaServiceImpl extends IBaseServiceImpl<WpPostMetaEntity, WpPostMetaMapper> implements IWpPostMetaService {
+
+}
