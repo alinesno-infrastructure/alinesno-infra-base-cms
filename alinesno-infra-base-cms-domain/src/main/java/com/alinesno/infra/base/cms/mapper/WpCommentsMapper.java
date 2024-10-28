322f5f5fdb05ed113ae2d3f8fd3210ccc0b927c7
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 添加 WpCommentsMapper 接口，用于文章评论的数据操作。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpCommentsMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpCommentsEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 文章评论
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpCommentsMapper extends IBaseMapper<WpCommentsEntity> {
+}
