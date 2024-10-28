8fa769c0c8912640e2e7cab547ecb93029a25d20
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:新增 WpLinksMapper 接口，用于链接实体的数据库操作。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpLinksMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpLinksEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 链接
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpLinksMapper extends IBaseMapper<WpLinksEntity> {
+}
