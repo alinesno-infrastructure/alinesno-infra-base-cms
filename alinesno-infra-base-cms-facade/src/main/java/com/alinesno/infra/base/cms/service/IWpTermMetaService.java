b6d6d2e6cabd154919041b51c51f9aea4f8ddabe
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:定义 IWpTermMetaService 接口，用于分类元数据的CRUD操作。

---
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/service/IWpTermMetaService.java
@@ -0,0 +1,13 @@
+// IWpTermMetaService.java
+package com.alinesno.infra.base.cms.service;
+
+import com.alinesno.infra.base.cms.entity.WpTermMetaEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * 分类元数据服务接口
+ * 该接口继承了基础服务接口IBaseService，用于对分类元数据实体(WpTermMetaEntity)进行CRUD操作
+ */
+public interface IWpTermMetaService extends IBaseService<WpTermMetaEntity> {
+ 
+}
