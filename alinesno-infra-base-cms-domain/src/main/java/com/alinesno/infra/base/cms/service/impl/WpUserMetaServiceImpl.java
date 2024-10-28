38ea2a3ea5049d5e21e3a00d7e42a9f88d17729f
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 实现 WpUserMetaServiceImpl 类，提供用户元数据服务实现。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpUserMetaServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpUserMetaEntity;
+import com.alinesno.infra.base.cms.mapper.WpUserMetaMapper;
+import com.alinesno.infra.base.cms.service.IWpUserMetaService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpUserMeta服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpUserMetaServiceImpl extends IBaseServiceImpl<WpUserMetaEntity, WpUserMetaMapper> implements IWpUserMetaService {
+
+}
