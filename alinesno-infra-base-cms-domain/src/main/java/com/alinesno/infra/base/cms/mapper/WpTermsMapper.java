4c3ed3d1529da071692dff2bff98bfa3a32f0f4d
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:新增 WpTermsMapper 接口，用于分类数据操作。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpTermsMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpTermsEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 分类
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpTermsMapper extends IBaseMapper<WpTermsEntity> {
+}
