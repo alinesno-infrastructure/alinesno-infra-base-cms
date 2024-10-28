992298f3775b5663b19822e20081cc5fa258bcb0
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    移除不必要的注解并更新作者信息

--- alinesno-infra-base-cms-boot/src/main/java/com/alinesno/infra/base/cms/BaseCmsApplication.java
+++ alinesno-infra-base-cms-boot/src/main/java/com/alinesno/infra/base/cms/BaseCmsApplication.java
@@ -1,19 +1,13 @@
 package com.alinesno.infra.base.cms;
 
-import com.alinesno.infra.common.facade.enable.EnableActable;
-import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 集成一个Java开发示例工具
- * @author LuoAnDong
- * @since 2023年8月3日 上午6:23:43
+ * @author luoxiaodong
+ * @version 1.0.0
  */
-@EnableActable
-@EnableInfraSsoApi
-@MapperScan("com.alinesno.infra.base.cms.mapper")
 @SpringBootApplication
 public class BaseCmsApplication {
 
