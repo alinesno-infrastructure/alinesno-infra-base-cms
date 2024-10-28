f6cebceea5cfb4543df58ead82728fe8d20b32e9
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:实现 WpCommentMetaServiceImpl 类，提供 WpCommentMeta 服务逻辑处理。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpCommentMetaServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpCommentMetaEntity;
+import com.alinesno.infra.base.cms.mapper.WpCommentMetaMapper;
+import com.alinesno.infra.base.cms.service.IWpCommentMetaService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpCommentMeta服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpCommentMetaServiceImpl extends IBaseServiceImpl<WpCommentMetaEntity, WpCommentMetaMapper> implements IWpCommentMetaService {
+
+}
