1d2c4e4c1dcea8be2b8894638fec6c105b9b0d4f
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:新增 WpUsersMapper 接口，用于用户数据操作。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpUsersMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpUsersEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 用户
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpUsersMapper extends IBaseMapper<WpUsersEntity> {
+}
