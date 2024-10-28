a18bd6e101c39d67d1dea553a2570aadbb2b1187
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:实现 WpTermsServiceImpl 类，提供 WpTerms 服务层处理功能。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpTermsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpTermsEntity;
+import com.alinesno.infra.base.cms.mapper.WpTermsMapper;
+import com.alinesno.infra.base.cms.service.IWpTermsService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpTerms服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpTermsServiceImpl extends IBaseServiceImpl<WpTermsEntity, WpTermsMapper> implements IWpTermsService {
+
+}
