34058ee5050e6400e1f0d97aa3b3aee268a0fe94
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 实现 WpTermTaxonomyServiceImpl 类，提供术语分类服务的业务逻辑处理。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpTermTaxonomyServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpTermTaxonomyEntity;
+import com.alinesno.infra.base.cms.mapper.WpTermTaxonomyMapper;
+import com.alinesno.infra.base.cms.service.IWpTermTaxonomyService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpTermTaxonomy服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpTermTaxonomyServiceImpl extends IBaseServiceImpl<WpTermTaxonomyEntity, WpTermTaxonomyMapper> implements IWpTermTaxonomyService {
+
+}
