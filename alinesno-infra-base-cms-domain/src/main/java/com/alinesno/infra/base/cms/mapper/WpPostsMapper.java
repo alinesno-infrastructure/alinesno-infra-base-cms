0e109130e4670d7ec4345c479344f152194aa43e
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 添加 WpPostsMapper 接口，用于文章实体的数据库操作。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpPostsMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpPostsEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 文章
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpPostsMapper extends IBaseMapper<WpPostsEntity> {
+}
