cb2da2c805eb6b30b0ce189e94702d69c03e9048
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 添加IWpLinksService接口，用于链接实体的CRUD操作。

---
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/service/IWpLinksService.java
@@ -0,0 +1,13 @@
+// IWpLinksService.java
+package com.alinesno.infra.base.cms.service;
+
+import com.alinesno.infra.base.cms.entity.WpLinksEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * 链接服务接口
+ * 该接口继承了基础服务接口IBaseService，用于对链接实体(WpLinksEntity)进行CRUD操作
+ */
+public interface IWpLinksService extends IBaseService<WpLinksEntity> {
+ 
+}
