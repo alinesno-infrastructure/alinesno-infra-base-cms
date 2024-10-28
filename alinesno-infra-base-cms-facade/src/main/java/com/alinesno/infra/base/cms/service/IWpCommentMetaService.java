ee42f475ef1fa4e19b658c6b6f0ab44e07031e3f
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 定义 IWpCommentMetaService 接口，用于评论元数据的CRUD操作。

---
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/service/IWpCommentMetaService.java
@@ -0,0 +1,13 @@
+// IWpCommentMetaService.java
+package com.alinesno.infra.base.cms.service;
+
+import com.alinesno.infra.base.cms.entity.WpCommentMetaEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * 评论元数据服务接口
+ * 该接口继承了基础服务接口IBaseService，用于对评论元数据实体(WpCommentMetaEntity)进行CRUD操作
+ */
+public interface IWpCommentMetaService extends IBaseService<WpCommentMetaEntity> {
+ 
+}
