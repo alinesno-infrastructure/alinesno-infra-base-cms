b754f41fced0656c72d257e5ede36e1af5980c8f
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 实现 WpOptions 服务类 WpOptionsServiceImpl

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpOptionsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpOptionsEntity;
+import com.alinesno.infra.base.cms.mapper.WpOptionsMapper;
+import com.alinesno.infra.base.cms.service.IWpOptionsService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpOptions服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpOptionsServiceImpl extends IBaseServiceImpl<WpOptionsEntity, WpOptionsMapper> implements IWpOptionsService {
+
+}
