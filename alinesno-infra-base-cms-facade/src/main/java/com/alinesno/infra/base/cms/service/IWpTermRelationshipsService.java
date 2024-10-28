ffb442bef410db6f8c7d432a9987e68b2db8b00b
赵阳(Yang) <yangAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:新增分类关系服务接口IWpTermRelationshipsService

---
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/service/IWpTermRelationshipsService.java
@@ -0,0 +1,13 @@
+// IWpTermRelationshipsService.java
+package com.alinesno.infra.base.cms.service;
+
+import com.alinesno.infra.base.cms.entity.WpTermRelationshipsEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * 分类关系服务接口
+ * 该接口继承了基础服务接口IBaseService，用于对分类关系实体(WpTermRelationshipsEntity)进行CRUD操作
+ */
+public interface IWpTermRelationshipsService extends IBaseService<WpTermRelationshipsEntity> {
+ 
+}
