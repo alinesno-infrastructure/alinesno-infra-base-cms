b57f5e478fa2ba253c545b277a5d02dbeea13f5b
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 添加 WpTermMetaMapper 接口，用于分类元数据操作。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpTermMetaMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpTermMetaEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 分类元数据
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpTermMetaMapper extends IBaseMapper<WpTermMetaEntity> {
+}
