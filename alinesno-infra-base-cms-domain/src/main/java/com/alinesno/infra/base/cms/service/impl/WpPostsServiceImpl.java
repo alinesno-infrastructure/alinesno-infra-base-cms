b2421bd7603e541dede1d9b0fab7bfa90e993b5f
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:新增 WpPostsServiceImpl 类，实现 IWpPostsService 接口。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpPostsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpPostsEntity;
+import com.alinesno.infra.base.cms.mapper.WpPostsMapper;
+import com.alinesno.infra.base.cms.service.IWpPostsService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpPosts服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpPostsServiceImpl extends IBaseServiceImpl<WpPostsEntity, WpPostsMapper> implements IWpPostsService {
+
+}
