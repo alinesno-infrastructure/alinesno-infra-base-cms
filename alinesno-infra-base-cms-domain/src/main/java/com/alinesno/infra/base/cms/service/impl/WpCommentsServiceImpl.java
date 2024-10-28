7c338002d4af456ebaf6c30f0fb2f5196ad91313
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:实现 WpCommentsServiceImpl 类，包含基本属性和服务接口实现。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpCommentsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpCommentsEntity;
+import com.alinesno.infra.base.cms.mapper.WpCommentsMapper;
+import com.alinesno.infra.base.cms.service.IWpCommentsService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpComments服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpCommentsServiceImpl extends IBaseServiceImpl<WpCommentsEntity, WpCommentsMapper> implements IWpCommentsService {
+
+}
