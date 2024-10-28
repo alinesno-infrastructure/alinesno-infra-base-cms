404a43fed4de35a7198c37032c0ba5c7363458c2
赵阳(Yang) <yangAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 添加分类关系映射接口 WpTermRelationshipsMapper

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpTermRelationshipsMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpTermRelationshipsEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 分类关系
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpTermRelationshipsMapper extends IBaseMapper<WpTermRelationshipsEntity> {
+}
