8aa2ea12b0d18003ef35b9a28ebc4ec29556ac60
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 添加 WpCommentMetaMapper 接口，用于文章评论额外信息的数据库操作。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpCommentMetaMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpCommentMetaEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 文章评论额外信息
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpCommentMetaMapper extends IBaseMapper<WpCommentMetaEntity> {
+}
