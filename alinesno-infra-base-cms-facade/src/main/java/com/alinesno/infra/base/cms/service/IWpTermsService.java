46b68ebfddf43f47200522fd9e795c6784e15aea
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:新增 IWpTermsService 接口，用于分类实体的CRUD操作。

---
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/service/IWpTermsService.java
@@ -0,0 +1,13 @@
+// IWpTermsService.java
+package com.alinesno.infra.base.cms.service;
+
+import com.alinesno.infra.base.cms.entity.WpTermsEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * 分类服务接口
+ * 该接口继承了基础服务接口IBaseService，用于对分类实体(WpTermsEntity)进行CRUD操作
+ */
+public interface IWpTermsService extends IBaseService<WpTermsEntity> {
+ 
+}
