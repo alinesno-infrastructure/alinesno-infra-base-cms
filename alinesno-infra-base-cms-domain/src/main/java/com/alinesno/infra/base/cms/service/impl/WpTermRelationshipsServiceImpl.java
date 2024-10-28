fd8c5323b5b081960e6e58f0230de41c35be1eaf
赵阳(Yang) <yangAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:实现 WpTermRelationships 服务类

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpTermRelationshipsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpTermRelationshipsEntity;
+import com.alinesno.infra.base.cms.mapper.WpTermRelationshipsMapper;
+import com.alinesno.infra.base.cms.service.IWpTermRelationshipsService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpTermRelationships服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpTermRelationshipsServiceImpl extends IBaseServiceImpl<WpTermRelationshipsEntity, WpTermRelationshipsMapper> implements IWpTermRelationshipsService {
+
+}
