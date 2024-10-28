5580cd3a4218c2c3c6a18690b849a739ce9a2d7d
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:新增文章元数据映射接口 WpPostMetaMapper

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/mapper/WpPostMetaMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.base.cms.mapper;
+
+import com.alinesno.infra.base.cms.entity.WpPostMetaEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 文章元数据
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface WpPostMetaMapper extends IBaseMapper<WpPostMetaEntity> {
+}
