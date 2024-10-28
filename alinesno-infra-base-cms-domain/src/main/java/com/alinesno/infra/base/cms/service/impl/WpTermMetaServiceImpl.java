576ba5365b632b283aaf6aae6ce87b07586f9d8c
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:实现 WpTermMetaServiceImpl 类，提供 WpTermMeta 服务层处理功能。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpTermMetaServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpTermMetaEntity;
+import com.alinesno.infra.base.cms.mapper.WpTermMetaMapper;
+import com.alinesno.infra.base.cms.service.IWpTermMetaService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpTermMeta服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpTermMetaServiceImpl extends IBaseServiceImpl<WpTermMetaEntity, WpTermMetaMapper> implements IWpTermMetaService {
+
+}
