4bbcdca5c551a7c1d0355071b8e6598fb0a7b6b5
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat: 添加 IWpTermTaxonomyService 接口，用于分类目录的CRUD操作。

---
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/service/IWpTermTaxonomyService.java
@@ -0,0 +1,13 @@
+// IWpTermTaxonomyService.java
+package com.alinesno.infra.base.cms.service;
+
+import com.alinesno.infra.base.cms.entity.WpTermTaxonomyEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * 分类目录服务接口
+ * 该接口继承了基础服务接口IBaseService，用于对分类目录实体(WpTermTaxonomyEntity)进行CRUD操作
+ */
+public interface IWpTermTaxonomyService extends IBaseService<WpTermTaxonomyEntity> {
+ 
+}
