1778efecadf6416a2dcf34614580bbc153eb4c9a
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:19:05 +0800

    feat:实现 WpLinksServiceImpl 类，提供链接管理服务逻辑处理。

---
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpLinksServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.base.cms.service.impl;
+
+import com.alinesno.infra.base.cms.entity.WpLinksEntity;
+import com.alinesno.infra.base.cms.mapper.WpLinksMapper;
+import com.alinesno.infra.base.cms.service.IWpLinksService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * WpLinks服务层处理
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@Service
+public class WpLinksServiceImpl extends IBaseServiceImpl<WpLinksEntity, WpLinksMapper> implements IWpLinksService {
+
+}
