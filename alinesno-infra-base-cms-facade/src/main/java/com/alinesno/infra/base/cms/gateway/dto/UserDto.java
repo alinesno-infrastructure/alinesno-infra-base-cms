5d394f5b04edd285a61d9423e8b241be134cef39
陈伟(Victor) <victorAip@gmail.com>
2024-10-30 00:13:12 +0800

    feat: 添加 UserDto 类，包含用户基本信息字段。

---
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/gateway/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.cms.gateway.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private String id;
+    private String avatar;
+    private String username;
+    private String description;
+
+}
