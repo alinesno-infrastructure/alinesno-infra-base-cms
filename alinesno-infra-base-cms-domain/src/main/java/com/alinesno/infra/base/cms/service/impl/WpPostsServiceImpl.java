c79a0bbecce4fda9f92ea677e7a342d22c6cf8bd
陈伟(Victor) <victorAip@gmail.com>
2024-10-30 00:13:12 +0800

    feat:在WpPostsServiceImpl中新增保存文章内容及附件信息的方法。

--- alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpPostsServiceImpl.java
+++ alinesno-infra-base-cms-domain/src/main/java/com/alinesno/infra/base/cms/service/impl/WpPostsServiceImpl.java
@@ -1,10 +1,15 @@
 package com.alinesno.infra.base.cms.service.impl;
 
+import com.alinesno.infra.base.cms.entity.WpPostMetaEntity;
 import com.alinesno.infra.base.cms.entity.WpPostsEntity;
+import com.alinesno.infra.base.cms.gateway.dto.ContentCardDto;
+import com.alinesno.infra.base.cms.gateway.enums.CardType;
+import com.alinesno.infra.base.cms.mapper.WpPostMetaMapper;
 import com.alinesno.infra.base.cms.mapper.WpPostsMapper;
 import com.alinesno.infra.base.cms.service.IWpPostsService;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +22,41 @@ import org.springframework.stereotype.Service;
 @Service
 public class WpPostsServiceImpl extends IBaseServiceImpl<WpPostsEntity, WpPostsMapper> implements IWpPostsService {
 
+    @Autowired
+    private WpPostMetaMapper wpPostMetaMapper ;
+
+    @Override
+    public void saveContent(ContentCardDto content) {
+
+        // 保存文章内容(post表)
+        WpPostsEntity wpPostsEntity = new WpPostsEntity();
+
+        wpPostsEntity.setPostStatus("publish");
+        wpPostsEntity.setPostAuthor(content.getUserId()) ;
+        wpPostsEntity.setPostContent(content.getContent());
+        wpPostsEntity.setPostTitle(content.getTitle());
+        wpPostsEntity.setPostExcerpt(content.getContent());
+
+        wpPostsEntity.setPostType(content.getType().name());
+
+        this.save(wpPostsEntity);
+
+        // 保存文章image/audio/video等附件信息(postmeta表)
+        long postId = wpPostsEntity.getId() ;
+        WpPostMetaEntity postMetaEntity = new WpPostMetaEntity();
+        postMetaEntity.setPostId(postId);
+
+        if(content.getType() == CardType.image){
+            postMetaEntity.setMetaKey("image");
+            postMetaEntity.setMetaValue(content.getImage());
+        }else if (content.getType() == CardType.music){
+            postMetaEntity.setMetaKey("audio");
+            postMetaEntity.setMetaValue(content.getAudioSrc());
+        }else if (content.getType() == CardType.video){
+            postMetaEntity.setMetaKey("video");
+            postMetaEntity.setMetaValue(content.getVideoSrc());
+        }
+
+        wpPostMetaMapper.insert(postMetaEntity);
+    }
 }
