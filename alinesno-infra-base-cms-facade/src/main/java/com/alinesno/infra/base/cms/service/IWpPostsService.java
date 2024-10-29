2e63c66e142f6d8796261f939ad16767d4b89bc4
陈伟(Victor) <victorAip@gmail.com>
2024-10-30 00:13:12 +0800

    feat:在IWpPostsService中新增保存文章内容的方法。

--- alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/service/IWpPostsService.java
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/service/IWpPostsService.java
@@ -2,6 +2,7 @@
 package com.alinesno.infra.base.cms.service;
 
 import com.alinesno.infra.base.cms.entity.WpPostsEntity;
+import com.alinesno.infra.base.cms.gateway.dto.ContentCardDto;
 import com.alinesno.infra.common.facade.services.IBaseService;
 
 /**
@@ -9,5 +10,11 @@ import com.alinesno.infra.common.facade.services.IBaseService;
  * 该接口继承了基础服务接口IBaseService，用于对文章实体(WpPostsEntity)进行CRUD操作
  */
 public interface IWpPostsService extends IBaseService<WpPostsEntity> {
- 
+
+    /**
+     * 保存文章内容
+     * @param content
+     */
+    void saveContent(ContentCardDto content);
+
 }
