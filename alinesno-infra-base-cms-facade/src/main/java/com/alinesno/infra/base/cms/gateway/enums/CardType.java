82010d87e0b1bf5e6671d8410b65e9f4a375a6ae
陈伟(Victor) <victorAip@gmail.com>
2024-10-30 00:13:12 +0800

    feat: 添加 CardType 枚举类，包含文章、音乐、视频和图片类型。

---
+++ alinesno-infra-base-cms-facade/src/main/java/com/alinesno/infra/base/cms/gateway/enums/CardType.java
@@ -0,0 +1,27 @@
+package com.alinesno.infra.base.cms.gateway.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum CardType {
+
+    article("article"),
+    music("music"),
+    video("video"),
+    image("image");
+
+    private final String value;
+
+    CardType(String value) {
+        this.value = value;
+    }
+
+    public static CardType fromValue(String value) {
+        for (CardType type : values()) {
+            if (type.getValue().equals(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid card type: " + value);
+    }
+}
